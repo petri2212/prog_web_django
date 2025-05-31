93438edbaea819eac2cfaa729b71d5018700daaa
Andrei <a.petrisor@studenti.unibg.it>
2025-05-31 23:10:12 +0200

    setting debug=false and allowed hosts

--- src/src/settings.py
+++ src/src/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*=q09qmz^jqnog6tm)o9m_5z2voy*vs%^(l836de)cv($(pw7+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
