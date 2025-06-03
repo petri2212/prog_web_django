81b6de19a99ae58486dbc24fbd7bf994f7c70809
petri <a.petrisor@studenti.unibg.it>
2025-06-03 16:35:50 +0200

    add run .venv on run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal
 
+:: Attiva ambiente virtuale (se usi venv)
+echo Attivazione ambiente virtuale...
+call .venv\Scripts\activate.bat
+
 :: Avvia il server
 echo Avvio del server Django su http://127.0.0.1:8000
 cmd /c start "" http://127.0.0.1:8000/museo/autore
